9c066805e16b2016157151b2f0de77f77bfb46cd
Preeti Nehte <preeti.nehte@globant.com>
2016-03-07 15:15:52 +0530

    sample Pod

---
+++ Pod/Classes/Subspec/TestSubSpec.swift
@@ -0,0 +1,13 @@
+//
+//  TestSubSpec.swift
+//  Pods
+//
+//  Created by Preeti Nehte on 07/03/16.
+//
+//
+
+import UIKit
+
+class TestSubSpec: NSObject {
+
+}
---
+++ Pod/Classes/TestSamplePod.swift
@@ -0,0 +1,13 @@
+//
+//  TestSamplePod.swift
+//  Pods
+//
+//  Created by Preeti Nehte on 07/03/16.
+//
+//
+
+import UIKit
+
+class TestSamplePod: NSObject {
+
+}
